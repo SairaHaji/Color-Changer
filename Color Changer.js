7b65dfd1d2d3c1fc2ffa3b76e2a65e13c24bbbaa
SairaHaji <88104760+SairaHaji@users.noreply.github.com>
2021-08-15 15:15:41 -0400

    Add files via upload

---
+++ Color Changer.js
@@ -0,0 +1,40 @@
+
+function setup() {
+  createCanvas(400,400);
+
+
+
+  
+
+ 
+
+}
+
+
+
+function draw() {
+
+  if (keyDown(DOWN_ARROW)) {
+    background ('red')
+  }
+
+
+  if (keyDown(UP_ARROW)) {
+    background ('purple')
+  }
+
+
+  if (keyDown(RIGHT_ARROW)) {
+    background ('yellow')
+  }
+
+
+  if (keyDown(LEFT_ARROW)) {
+    background ('LightSeaGreen')
+  }
+  
+
+}
+
+
+
